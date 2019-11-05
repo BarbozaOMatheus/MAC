2ed85c363f64292b55875b43858cc49630945b2e
Matheus Barboza <matheus.barboza102@outlook.com>
2019-11-05 15:21:22 -0300

    Atualização

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,52 +8,52 @@ import styles from './estilo';
 export default class Main extends Component {
 
   render() {
-    return(
+    return (
       <View style={styles.conainer}>
         <View style={styles.top}>
-        
-            <TouchableOpacity style={styles.profileImage}
+
+          <TouchableOpacity style={styles.profileImage}
             onPress={() => this.props.navigation.goBack()}
-            >
-              <Icon name="arrow-left" size={40} color="#fabb00" />
-            </TouchableOpacity>
-          
-            <TouchableOpacity style={styles.profileImageConfig}
-            onPress={() => this.props.navigation.navigate('Config')}
-            >
-              <Icon name="cog" size={40} color="#0e6ee3"/>
-            </TouchableOpacity>  
-        </View>  
-        
+          >
+            <Icon name="arrow-left" size={40} color="#fabb00" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.profileImageConfig}
+            onLongPress={() => this.props.navigation.navigate('Config')}
+          >
+            <Icon name="cog" size={40} color="#0e6ee3" />
+          </TouchableOpacity>
+        </View>
+
         <View style={styles.center}></View>
-        
+
         <View style={styles.bottom}>
           <View style={styles.bottomItem}>
             <TouchableOpacity style={styles.bottomItemInner}>
-              <Icon name="phone" size={65} color="red"/>
+              <Icon name="phone" size={65} color="red" />
             </TouchableOpacity>
           </View>
-          
+
           <View style={styles.bottomItem}>
             <TouchableOpacity style={styles.bottomItemInner}>
-              <Icon name="address-book" size={65} color="black"/>
+              <Icon name="address-book" size={65} color="black" />
             </TouchableOpacity>
           </View>
-          
+
           <View style={styles.bottomItem}>
             <TouchableOpacity style={styles.bottomItemInner}>
-              <Icon name="google" size={65} color="#0e6ee3"/>
+              <Icon name="google" size={65} color="#0e6ee3" />
             </TouchableOpacity>
           </View>
-          
+
           <View style={styles.bottomItem}>
             <TouchableOpacity style={styles.bottomItemInner}>
-              <Icon name="whatsapp" size={65} color="#34af23"/>
+              <Icon name="whatsapp" size={65} color="#34af23" />
             </TouchableOpacity>
           </View>
         </View>
-        
-      </View> 
+
+      </View>
     );
   }
 }
